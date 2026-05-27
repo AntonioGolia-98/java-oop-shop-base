468774f3b5d73bccadf71b8e981ef171bbc1eb17
AntonioGolia-98 <antoniog98@icloud.com>
2026-05-27 18:48:07 +0200

    creazione random codice

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -1,8 +1,20 @@
+import java.util.Random;
+
 public class Prodotto {
 
     public int codice;
     public String nome;
     public String costruzione;
-    public int prezzo;
-    public int iva;
+    public double prezzo;
+    public double iva;
+
+    public Prodotto(String nome, String costruzione, double prezzo, double iva) {
+        Random random = new Random();
+        this.codice = random.nextInt(99999) + 1;
+        this.nome = nome;
+        this.costruzione = costruzione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
 }
